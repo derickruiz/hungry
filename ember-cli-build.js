5bed8e6883e22b85bea6a47b5af94ccf53a79d6e
itsderick <derickiscool@gmail.com>
2016-08-29 16:11:30 -0500

    Adding in jQuery to take care of Ajax requests.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('/vendor/jquery-3.1.0.min.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //

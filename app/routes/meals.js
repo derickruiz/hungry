c552a1b8ab99e82df90ce4d7e35d3a8a6635a702
itsderick <derickiscool@gmail.com>
2016-08-29 17:11:17 -0500

    Setting some data on the controller and updating the default id so the url can look good.

--- app/routes/meals.js
+++ app/routes/meals.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+    setupController(controller, model) {
+        this._super(...arguments);
+        controller.set('menuItems', model.map((entry) => {
+            entry.menu_items[0].oldId = entry.menu_items[0].id;
+            entry.menu_items[0].id = entry.menu_items[0].name.toLowerCase().split(" ").join("-");
+            return entry.menu_items[0];
+        }));
+    },
     model() {
         return $.get('https://api.tryhungry.com/api/v1/search.json?latitude=38.896127&longitude=-77.0735961').then((raw) => {
             return raw;

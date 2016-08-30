c44a2fedaa33fe2e6e58c7725c4f8f72138e5e3d
itsderick <derickiscool@gmail.com>
2016-08-30 01:26:22 -0500

    Doing another ajax request for the data here in meal. The reason is because if this route is visited directly it won't have the data picked up from the parent route. That can surely be fixed in the future with Ember Data.

--- app/routes/meal.js
+++ app/routes/meal.js
@@ -1,10 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    setupController(controller, model) {
-        
-    },
-    model() {
-        let meals = this.modelFor('meals');
+    model(params) {
+
+        var id = params.id,
+            returnObj = {};
+
+        return $.get('https://api.tryhungry.com/api/v1/search.json?latitude=38.896127&longitude=-77.0735961').then((raw) => {
+
+            let modifiedRaw = raw.map(function (entry) {
+                entry.menu_items[0].oldId = entry.menu_items[0].id;
+                entry.menu_items[0].id = entry.menu_items[0].name.toLowerCase().split(" ").join("-");
+                return entry;
+            });
+
+            modifiedRaw.forEach(function (entry) {
+                if (id === entry.menu_items[0].id) {
+                    console.log("Is id matching up?");
+                    returnObj.menuItem = entry.menu_items[0];
+                    returnObj.chef = entry.chef;
+                    returnObj.ingredients = {
+                        ingredients_image: entry.menu_items[0].ingredients_image,
+                        ingredients_text: entry.menu_items[0].ingredients_text
+                    };
+                    return;
+                }
+            });
+
+            return returnObj;
+
+        });
     }
 });

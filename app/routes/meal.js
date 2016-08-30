9ce74767cade40a77ba0f9a2aa16d75c9dd05349
itsderick <derickiscool@gmail.com>
2016-08-29 22:11:01 -0500

    Setting up initial data on individual route.

--- app/routes/meal.js
+++ app/routes/meal.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    setupController(controller, model) {
+        
+    },
+    model() {
+        let meals = this.modelFor('meals');
+    }
 });

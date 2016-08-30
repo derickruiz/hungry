256eb15bda78a4ac3215a1ba9853de1ab53011ce
itsderick <derickiscool@gmail.com>
2016-08-30 02:28:47 -0500

    Making a few changes to components, route templates, and styles to account for some items being sold out.

--- app/components/meal-item.js
+++ app/components/meal-item.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-    classNames: ['Meal']
+const MealItemComponent = Ember.Component.extend({
+    classNames: ['Meal'],
+    classNameBindings: ['isSoldOut'],
+    isSoldOut: Ember.computed('available_quantity', function(){
+        return this.get('available_quantity') <= 0 ? true : false;
+    })
 });
+
+MealItemComponent.reopenClass({
+  positionalParams: ['photo', 'available_quantity', 'name', 'description', 'price', 'rating', 'reviews_count', 'id']
+});
+
+export default MealItemComponent;

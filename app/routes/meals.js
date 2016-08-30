7f7055461fd371325597d97d6f9300c7dad2bd4c
itsderick <derickiscool@gmail.com>
2016-08-30 12:14:25 -0500

    Making sure the chef headshot image is in the returned object.

--- app/routes/meals.js
+++ app/routes/meals.js
@@ -7,6 +7,7 @@ export default Ember.Route.extend({
             return raw.map((entry) => {
                 entry.menu_items[0].oldId = entry.menu_items[0].id;
                 entry.menu_items[0].id = entry.menu_items[0].name.toLowerCase().split(" ").join("-");
+                entry.menu_items[0].chefImage = entry.chef.chef_profile.headshot_image.url;
                 return entry.menu_items[0];
             });
 
